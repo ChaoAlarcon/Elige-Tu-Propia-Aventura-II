d04e0eab92e2ab3a6a685171d9cc3004d62d1ce5
Nicolás-1400 <nicolasgarciasampedrodocampo@gmail.com>
2025-05-23 11:14:04 +0200

    Intento de implementar la base de datos

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -1,5 +1,10 @@
 package main;
 
 public class Menu {
-
+	public void mostrarMenu() {
+		System.out.println("¿Que quieres hacer?");
+		System.out.println("1. Jugar");
+		System.out.println("2.Mostrar puntuaciones");
+		
+	}
 }
--- src/main/main.java
+++ src/main/main.java
@@ -1,10 +1,17 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection connect = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/etpa2_g25v2", "root", "");
+		System.out.println("Conexión establecida");
+		Menu menu = new Menu();	
+		menu.mostrarMenu();
 	}
 
 }
